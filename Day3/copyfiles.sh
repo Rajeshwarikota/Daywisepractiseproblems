40ab624784b9248a42457bd6e6de72cdc6952111
Rajeshwarikota <kotarajeswari003.com>
2022-12-28 09:19:30 +0530

    Added: Day3 practice problem

---
+++ Day3/copyfiles.sh
@@ -0,0 +1,19 @@
+
+
+#/!/bin/bash -x
+
+
+for file in ls *.txt
+do
+       
+	folderName=`echo $files | awk -F. '{print $1}'`;
+	echo $files;
+	echo $folderName;
+	printf "\n";
+	if [ -d $folderName ]
+	then
+		rm -r $folderName;
+	fi
+	mkdir $folderName;
+	cp $files $folderName/$files;
+done
